76c1694263c9e4e531f9400fb469a0d9bd2ed6b2
saleh <salehsakil12@gmail.com>
2015-07-27 20:23:41 +0600

    added dijkstra algo

---
+++ src/Dijkstra.java
@@ -0,0 +1,19 @@
+import com.SR.code.Program;
+
+class Base {
+    String st = "default";
+    public void print() {
+        System.out.println(st);
+    }
+    public void setSt(String st) {
+        this.st = st;
+    }
+}
+
+public class Dijkstra {
+    public  static void main(String args[]) {
+        Base ob = new Base();
+        Main mn = new Main();
+        Program prog = new Program();
+    }
+}
